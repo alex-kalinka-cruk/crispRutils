2d726d9c255fb8df2d2007dcf97cc7b7e2079fca
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2020-04-20 23:23:56 +0100

    PRROC changes.

--- R/get_PRROC.R
+++ R/get_PRROC.R
@@ -1,16 +1,16 @@
-#' add_PRROC
+#' get_PRROC
 #'
-#' Adds precision and recall columns to a data frame containing TP and a score metric (e.g. Bayes factor).
+#' Returns precision and recall values for a data frame containing TP and a score metric (e.g. Bayes factor), or the AUPrRc.
 #'
 #' @param data A data frame containing a 'TP' column.
 #' @param score_col A character string naming a column containing a score metric.
 #' @param return A character string indicating what should be returned: if `curve` then the PR curve is added to the data frame, if `auc` then the AUCPrRc value is returned.
 #'
 #' @return A data frame or a single numeric value.
-#' @import dplyr mutate filter select
+#' @import dplyr mutate filter select rename
 #' @import PRROC pr.curve
 #' @export
-add_PRROC <- function(data, score_col, return = "curve"){
+get_PRROC <- function(data, score_col, return = "curve"){
   if(!"TP" %in% colnames(data))
     stop("expecting to find a 'TP' column in 'data'")
   if(!score_col %in% colnames(data))
@@ -23,11 +23,10 @@ add_PRROC <- function(data, score_col, return = "curve"){
                      dplyr::filter(!TP) %>%
                      dplyr::select(!!sc))[,1])
   pr <- PRROC::pr.curve(pos_scores, neg_scores, curve = T)
-  if(return = "curve"){
-    prc <- as.data.frame(pr$curve)
-    data %<>%
-      dplyr::mutate(Recall = prc$V1, Precision = prc$V2)
-    return(data)
+  if(return == "curve"){
+    prc <- as.data.frame(pr$curve) %>%
+      dplyr::rename(Recall = V1, Precision = V2)
+    return(prc)
   }else{
     return(pr$auc.davis.goadrich)
   }
