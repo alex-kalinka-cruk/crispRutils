3ce531581eabe08be353a712ded7d51c983bbef1
Alex Kalinka <Alex.Kalinka@cancer.org.uk>
2020-10-06 17:37:01 +0100

    crispr libs annot data.

--- R/qc_plasmid_samples.R
+++ R/qc_plasmid_samples.R
@@ -9,11 +9,14 @@
 #' @importFrom dplyr %>% summarise group_by ungroup n
 #' @importFrom moments skewness
 #' @importFrom energy dcor2d
+#' @importFrom stats median mad
 qc_plasmid_samples <- function(data){
   tryCatch({
     qc <- data %>%
       dplyr::group_by(library, batch) %>%
       dplyr::summarise(Library_width = crispRutils::calculate_library_width(count),
+                       Coeff_Var_counts = 100*sd(count)/mean(count),
+                       Coeff_Var_counts.robust = 100*mad(count)/median(count),
                        Skewness = moments::skewness(count),
                        Sarles_bimodality = crispRutils::calculate_sarles_bimodality(count),
                        percent_zero_count_guides = 100*sum(count == 0)/dplyr::n(),
